877d397cde8ce5242ec17c0106f3053c5503ab3c
UmiKami <ggoku02@gmail.com>
2023-02-26 23:00:41 -0500

    fixed test

--- src/tests/routes_test.py
+++ src/tests/routes_test.py
@@ -23,4 +23,4 @@ def test_home():
     with client:
         response = client.get('/')
         assert response.status_code == 200
-        assert response.data == b'Home sweet home! <strong> Your Yoga App! <strong>'
+        assert response.data == b'Home sweet home! <strong> Your Yoga App! | new pipeline | <strong>'
